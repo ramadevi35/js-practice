efd18e192b2a70ddfedcd19823bb576f7613730e
ramadevi35 <gorlerama1992@gmail.com>
2024-12-13 13:02:42 +0530

    second commit

---
+++ counter program/Random number generator/index.js
@@ -0,0 +1,30 @@
+// const min = 50;
+// const max = 100;
+
+// let randomNum = Math.floor(Math.random() * (max - min)) + min;
+
+// console.log(randomNum);
+
+// RANDOM NUMBER GENERATOR:
+
+const mybutton = document.getElementById("mybutton");
+const mylabel1 = document.getElementById("mylabel1");
+const mylabel2 = document.getElementById("mylabel2");
+const mylabel3 = document.getElementById("mylabel3");
+
+const min = 1;
+const max = 6;
+
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+mybutton.onclick = function () {
+  randomNum1 = Math.floor(Math.random() * max) + min;
+  randomNum2 = Math.floor(Math.random() * max) + min;
+  randomNum3 = Math.floor(Math.random() * max) + min;
+
+  mylabel1.textContent = randomNum1;
+  mylabel2.textContent = randomNum2;
+  mylabel3.textContent = randomNum3;
+};
--- counter program/index.js
+++ counter program/index.js
@@ -23,23 +23,23 @@
 // console.log(Math.PI);
 // console.log(Math.E);
 
-let x = 5;
-let y = 3;
-let z = 1;
+// let x = 5;
+// let y = 3;
+// let z = 1;
 
-z = Math.round(x);
-z = Math.floor(x);
-z = Math.ceil(x);
-z = Math.trunc(x);
-z = Math.pow(y, x);
-z = Math.sqrt(x);
-z = Math.log(x);
-z = Math.sin(x);
-z = Math.cos(x);
-z = Math.tan(x);
-z = Math.abs(x);
-z = Math.sign(x);
-let max = Math.max(x, y, z);
-let min = Math.min(x, y, z);
+// z = Math.round(x);
+// z = Math.floor(x);
+// z = Math.ceil(x);
+// z = Math.trunc(x);
+// z = Math.pow(y, x);
+// z = Math.sqrt(x);
+// z = Math.log(x);
+// z = Math.sin(x);
+// z = Math.cos(x);
+// z = Math.tan(x);
+// z = Math.abs(x);
+// z = Math.sign(x);
+// let max = Math.max(x, y, z);
+// let min = Math.min(x, y, z);
 
-console.log(min);
+// console.log(min);
